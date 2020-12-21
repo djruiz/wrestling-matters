ae72856d61e322350fec621131185f228be9d8ea
Daniel Ruiz <djruiz@umail.iu.edu>
2020-12-21 17:11:56 -0500

    wrestling-matters

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,16 +1,10 @@
-import {Button, Box} from '@material-ui/core/'
 import './home.css'
 
 function Home() {
     return (
-            <Box style={{height: '100vh'}} bgcolor="text.primary">
-                <div className="home-page">
-                    <h1>Wrestling Matters Podcast</h1>
-                    <Button variant="contained" color="primary">
-                        Listen to latest episode
-                    </Button>
-                </div>
-            </Box>
+            <div>
+                <h1>Wrestling Matters</h1>
+            </div>
         
         
     );
